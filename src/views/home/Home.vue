c6f3ae24d86996eda1fe493b8488d7b2e091792c
松坂砂糖 <2365999861@qq.com>
2021-12-30 20:14:07 +0800

    轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 
 <script>
 import NavBar from "@/components/common/navbar/NavBar";
+import HomeSwiper from "./childComps/HomeSwiper.vue";
+import RecommendView from "./childComps/RecommendView.vue";
 import { getHomeMultidata } from "@/network/home.js";
 export default {
   name: "Home",
@@ -17,6 +21,8 @@ export default {
   },
   components: {
     NavBar,
+    HomeSwiper,
+    RecommendView,
   },
   created() {
     // 在组件创建完成就发送请求
