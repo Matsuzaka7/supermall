041efc7b7b9c9f66e65e6be34ca3a18b2ce0c65e
松坂砂糖 <2365999861@qq.com>
2021-12-31 13:52:33 +0800

    feature

---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="feayture">
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="@/assets/img/home/recommend_bg.jpg" alt="" />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FeatureView",
+};
+</script>
+
+<style>
+.feayture img {
+  width: 100%;
+}
+</style>
